9395e6a3f826284d50499e72f72211f2d816badd
Tudzy <narongsak5az@gmail.com>
2023-12-21 08:48:29 +0700

    start usedcar

--- usedcar/src/test/java/com/example/phooms/usedcar/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.phooms.usedcar.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.phooms.usedcar.controller.Car;
-
-public interface CarRepository extends CrudRepository<Car, Integer> {
-}
